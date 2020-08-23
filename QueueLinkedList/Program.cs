b7b39a8da201f9da6bbfea478657c11ec9ef67b0
Amruta Jahagirdar <ajahagirdar8@gmail.com>
2020-08-23 20:04:10 +0530

    first commit

---
+++ QueueLinkedList/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace QueueLinkedList
+{
+	class MainClass
+	{
+		public static void Main (string[] args)
+		{
+			QueueLL qll = new QueueLL ();
+			qll.Enqueue ("1");
+			qll.Enqueue ("2");
+			qll.Enqueue ("3");
+			qll.Enqueue ("4");
+			qll.Enqueue ("5");
+			qll.Enqueue ("6");
+
+			Console.WriteLine ("Dequeue :"+qll.Dequeue());
+			Console.WriteLine ("Dequeue :"+qll.Dequeue());
+			Console.WriteLine ("Dequeue :"+qll.Dequeue());
+			Console.WriteLine ("Dequeue :"+qll.Dequeue());
+			Console.WriteLine ("Dequeue :"+qll.Dequeue());
+			Console.WriteLine ("Dequeue :"+qll.Dequeue());
+			Console.WriteLine ("Dequeue :"+qll.Dequeue());
+
+			qll.Enqueue ("1");
+			qll.Enqueue ("5");
+			qll.Enqueue ("6");
+			Console.WriteLine ("Dequeue :"+qll.Dequeue());
+			Console.WriteLine ("Dequeue :"+qll.Dequeue());
+			Console.WriteLine ("Dequeue :"+qll.Dequeue());
+
+			//Console.WriteLine ("Hello World!");
+		}
+	}
+}
---
+++ QueueLinkedList/QueueLL.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace QueueLinkedList
+{
+	public class QueueLL
+	{
+		Node firstNode ,lastNode;
+
+		public int nodeCount{ get; set; }
+
+		public QueueLL()
+		{
+			//firstNode = lastNode = new Node ();
+		}
+
+		public class Node
+		{
+			public string data { get; set;}
+			public Node node{ get; set;}
+		}
+
+
+		public void Enqueue(string item)
+		{
+			Node newNode = new Node ();
+			newNode.data = item;
+
+			if (nodeCount == 0) {
+				firstNode = lastNode = newNode;
+				newNode.node = null;
+
+			} else {
+				Node oldLastnode = lastNode;
+
+				oldLastnode.node = newNode;
+
+				lastNode = newNode;
+			}
+			nodeCount++;
+		}
+
+		public string Dequeue()
+		{
+			string item;
+
+			if (nodeCount == 0) {
+				item = null;
+
+			}
+			else {
+
+				Node oldFirst = firstNode;
+				item = oldFirst.data;
+
+				firstNode = oldFirst.node;
+
+				oldFirst = null;
+				nodeCount--;
+			}
+
+			return item;
+		}
+
+	}
+}
+
